5eecc1bdb3829bfab4155801d2fe69d4155e4e48
Siarhei Haurylchyk <sggavrilchik@gmail.com>
2023-12-18 15:31:23 +0300

    cange Dialogs comp and add Dialogs.module.css

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import style from './Dialogs.module.css'
 
 function Dialogs() {
     return (
-        <div>
-            Dialogs
-        </div>
+
+            <div className={style.dialogs}>
+                <div className={style.dialogItems}>
+                    <div className={style.dialog}>Andrey</div>
+                    <div className={style.dialog}>Sergey</div>
+                    <div className={style.dialog}>Sacha</div>
+                    <div className={style.dialog}>Natacha</div>
+                </div>
+                <div className={style.dialogsMesage}>
+                    <div className={style.dialogMessage}>Привет</div>
+                    <div className={style.dialogMessage}>Как дела?</div>
+                    <div className={style.dialogMessage}>Что нового?</div>
+                    <div className={style.dialogMessage}>Как прошел день?</div>
+                </div>
+            </div>
+
     );
 }
 
